5454fb6c4a3c75391c10e8c86a449f8448d14dee
seshasaikrishnachanumolu <seshasaikrishnachanumolu@gmail.com>
2024-11-14 19:18:36 +0530

    Update index.js

--- crudmvc_server-master/index.js
+++ crudmvc_server-master/index.js
@@ -11,12 +11,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: "*",
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
+app.use(cors());
 
 app.use(bodyParser.json());
 
